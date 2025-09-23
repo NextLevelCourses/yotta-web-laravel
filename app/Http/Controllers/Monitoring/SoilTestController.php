4191cab94859b9c9f51c055ba31b7bad6d30c72c
xamppsul <samsul.marif100@gmail.com>
2025-09-23 18:15:15 +0800

    show naming device

--- app/Http/Controllers/Monitoring/SoilTestController.php
+++ app/Http/Controllers/Monitoring/SoilTestController.php
@@ -97,6 +97,7 @@ class SoilTestController extends Controller
         //mapping data to array
         $data = array(
             'labels' => ['08:00', '10:00', '12:00', '14:00', '16:00', '18:00'],
+            'device' => SoilTest::whereNotNull('device_id')->first(),
             'temperature' => $temperatures,
             'humidity' => $humaditys,
             'ec' => $ecs,
--- resources/views/monitoring/soil-test.blade.php
+++ resources/views/monitoring/soil-test.blade.php
@@ -3,7 +3,9 @@
 @section('content')
     <div class="container-fluid py-4">
         <div class="d-flex align-items-center justify-content-between mb-4">
-            <h5 class="m-0 text-muted">Dashboard / <span class="fw-bold text-primary">Soil Test</span></h5>
+            <h5 class="m-0 text-muted">Dashboard / <span class="fw-bold text-primary">Soil Test:
+                </span>{{ $data['device']->device_id }}
+            </h5>
             <div class="d-flex align-items-center gap-2">
                 @if (Auth::user()->isAdmin())
                     <select class="form-select form-select-sm" name="export-month-select" id="export-month-select"
