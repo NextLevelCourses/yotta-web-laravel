0ba29c7493b18efc14a3ce53925cb2e9f73a0c59
xamppsul <samsul.marif100@gmail.com>
2025-09-28 16:51:53 +0800

    grafik show realtime data

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,9 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    //constanta
+    const LATEST = 'desc';
+    const OLDEST = 'asc';
+    //interface
+    abstract public function HandleGetDataGrafikSoilTest(string $param, string $column, string $sort = 'asc'): array;
 }
