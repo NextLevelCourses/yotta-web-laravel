c195645a924aa99d0b4f90e651f4de9e357c07b2
xamppsul <samsul.marif100@gmail.com>
2025-11-01 15:59:32 +0800

    models: include param of fillable mass assigment

--- app/Models/Solar_dome.php
+++ app/Models/Solar_dome.php
@@ -4,4 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Solar_dome extends Model {}
+class Solar_dome extends Model
+{
+    protected $fillable = [
+        'temperature',
+        'humidity',
+        'controlMode',
+        'relayState',
+        'targetHumidity',
+        'measure_at',
+        'created_at',
+    ];
+
+    protected $casts = [
+        'measure_at' => 'datetime',
+    ];
+}
