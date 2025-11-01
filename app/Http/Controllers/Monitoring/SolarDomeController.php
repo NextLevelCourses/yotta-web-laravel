04a9615e57d9a23a79159915741a4dca51408477
xamppsul <samsul.marif100@gmail.com>
2025-11-01 16:57:29 +0800

    solardome: make interface get data of database realtime

--- app/Http/Controllers/Monitoring/SolarDomeController.php
+++ app/Http/Controllers/Monitoring/SolarDomeController.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Maatwebsite\Excel\Facades\Excel;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Carbon\Carbon;
 
 class SolarDomeController extends Controller implements ExportDataInterface
 {
@@ -27,12 +28,40 @@ class SolarDomeController extends Controller implements ExportDataInterface
         return Auth::check() ? view('monitoring.solar-dome', compact('dummyData')) : redirect()->route('login')->with('error', 'Anda perlu login,silahkan login!');
     }
 
+    private static function HandleGetDataSolarDome($data, int $key): array
+    {
+        switch ($key) {
+            case 0:
+                return array(
+                    "controlMode" => $data[$key]['controlMode'] ?? 0,
+                    "targetHumidity" => $data[$key]['targetHumidity'] ?? '-',
+                );
+                break;
+            case 1:
+                return array(
+                    "humidity" => $data[$key]['humidity'] ?? 0,
+                    "lastUpdate" => Carbon::createFromTimestamp($data[$key]['lastUpdate'] / 1000, config('app.timezone'))->toDateTimeString() ?? '-',
+                    // "lastUpdate" => $data[$key]['lastUpdate'] ?? '-',
+                    "relayState" => $data[$key]['relayState'] ?? '-',
+                    "temperature" => $data[$key]['temperature'] ?? 0,
+                );
+            default:
+                return array('message' => 'invalid key provided');
+                break;
+        }
+    }
+
     public function test_snapshot()
     {
         $db = app('firebase.database.solar_dome');
         $snapshot = $db->getReference(config('firebase.database.solar_dome.table'))->getValue();
-        // $snapshot = $this->database->getReference($this->table)->getValue();
-        return response()->json($snapshot);
+
+        $solarDomeData = [];
+        foreach ($snapshot as $data) {
+            array_push($solarDomeData, $data);
+        }
+
+        return $this->HandleGetDataSolarDome($solarDomeData, 1);
     }
 
     public function ExportByExcel(string $date): BinaryFileResponse|string
